42ce8b7ff4cea2b16fdb591fa78faf7a759c57a5
Leilani Gilpin <lgilpin@mit.edu>
2021-06-16 18:25:41 -0400

    Adding OO

--- commonsense/conceptnet.py
+++ commonsense/conceptnet.py
@@ -144,7 +144,7 @@ class ConceptNet(KB):
         return cleaned.replace(' ', '_')
 
 
-    def make_prolog_fact(triple, reason):
+    def make_prolog_fact(self, triple, reason):
         """
         Makes facts in the prolog style
         """
@@ -166,7 +166,7 @@ class ConceptNet(KB):
                 logging.debug("anchor point %s is partof the concept phrase: %s"
                               % (anchor, concept_phrase))
                 triple = [concept_phrase, 'IsA', anchor]
-                return make_fact(triple, "direct search")
+                return self.make_fact(triple, "direct search")
 
         for anchor in anchors:
             if type(concept_phrase) is list:
@@ -175,7 +175,7 @@ class ConceptNet(KB):
                 concept = concept_phrase
             return self.get_closest_anchor(concept, anchors, 'IsA')
 
-    def find_anchor_with_score(concept_phrase, anchors, include_score: bool = False):
+    def find_anchor_with_score(self, concept_phrase, anchors, include_score: bool = False):
         """
         Search for a specific anchor from a set of anchors
         TODO: include_score for the conceptNet weights
@@ -201,19 +201,19 @@ class ConceptNet(KB):
 
 
 
-    def aggregate(fact_term, relations):
+    def aggregate(self, fact_term, relations):
         """
         Aggregates a commonsense reasons for a particular concept and the relations of interest
         """
         all_facts = []
         for relation in relations:
-            concept_phrase = clean(fact_term)
-            new_facts = build_relation(concept_phrase, relation)
+            concept_phrase = self.clean(fact_term)
+            new_facts = self.build_relation(concept_phrase, relation)
             all_facts += new_facts
         return all_facts
 
     ########### Is this how we want the cleaning ###########################
-    def clean(symbolic_phrase):
+    def clean(self, symbolic_phrase):
         """
         Helper function for cleaning the symbols.
 
@@ -226,7 +226,7 @@ class ConceptNet(KB):
             return str_phrase
 
 
-    def build_relation(phrase, relation):
+    def build_relation(self, phrase, relation):
         facts = []
 
         concept = phrase  # Might want to get the last one, not so sure right now.
@@ -235,7 +235,7 @@ class ConceptNet(KB):
         for edge in edges:
             if edge['rel']['label'] == relation:
                 fact = edge['end']['label'].lower()
-                new_fact = make_fact([phrase, relation, fact], "ConceptNet")
+                new_fact = self.make_fact([phrase, relation, fact], "ConceptNet")
                 if new_fact not in facts:
                     facts.append(new_fact)
         return facts
@@ -281,7 +281,7 @@ class ConceptNet(KB):
         return False
 
 
-    def clean_concept(word):
+    def clean_concept(self,word):
         """
         Remove dashes into a phrase
         """
@@ -295,11 +295,11 @@ class ConceptNet(KB):
     # Counts the amount of IsA hops from a start to an anchor point
     # This should be a shortest path algorithm
 
-    def get_shortest_hops(start, relation='IsA'):
+    def get_shortest_hops(self, start, relation='IsA'):
         shortest = None
         target = None
         for anchor in subject_anchors:  # Will want to toggle for verb
-            candidate = find_shortest_path(start, anchor, relation)
+            candidate = self.find_shortest_path(start, anchor, relation)
             try:
                 if candidate and not shortest or len(candidate) < len(shortest):
                     shortest = candidate
@@ -311,28 +311,28 @@ class ConceptNet(KB):
 
     # Finds the shortest path for a specificed relation
 
-    def find_shortest_path(start, anchor, relation='IsA', path=None):
+    def find_shortest_path(self, start, anchor, relation='IsA', path=None):
         if path == None:
             path = []
         path = path + [start]
-        if has_IsA_edge(start, anchor):
+        if self.has_IsA_edge(start, anchor):
             return path + [anchor]
         if len(path) >= MAX_DEPTH:
             return None
         shortest = None
-        search = clean_search(start)
+        search = self.clean_search(start)
         obj = requests.get(query_prefix + search + isA_search).json()
         edges = obj['edges']
 
         for edge in edges:
-            from_node = clean_search(edge['start']['label'])
-            to_node = clean_search(edge['end']['label'])
+            from_node = self.clean_search(edge['start']['label'])
+            to_node = self.clean_search(edge['end']['label'])
             rel = edge['rel']['label']
 
             # May need more processing
-            if (search_equals(from_node, search) and rel == relation):
+            if (self.search_equals(from_node, search) and rel == relation):
                 if to_node not in path:
-                    newpath = find_shortest_path(to_node, anchor, relation, path)
+                    newpath = self.find_shortest_path(to_node, anchor, relation, path)
                     if newpath:
                         if not shortest or len(newpath) < len(shortest):
                             shortest = newpath
@@ -340,14 +340,12 @@ class ConceptNet(KB):
 
 
 
-    def search_equals(string1, string2):
-        if (clean_search(string1) == clean_search(string2)):
+    def search_equals(self, string1, string2):
+        if (self.clean_search(string1) == self.clean_search(string2)):
             return True
         return False
 
-
-
-    def clean_search(input):
+    def clean_search(self, input):
         cleaned = input.lower()
         if (cleaned.startswith("a ")):
             cleaned = cleaned.replace("a ", "", 1)
@@ -359,7 +357,7 @@ class ConceptNet(KB):
         for (start,end) in combinations(labels,2):
             x = str(start)
             y = str(end)
-            if has_any_edge(x,y):
+            if self.has_any_edge(x,y):
                 continue
             else:
                 print("found no direct edge between %s and %s"%(x,y))
@@ -374,7 +372,7 @@ class ConceptNet(KB):
     #
     # Only to be used
     # for verb primitives, otherwise not strong enough correlation
-    def has_any_edge(word, verb_primitive, verbose=False):
+    def has_any_edge(self, word, verb_primitive, verbose=False):
         word_text = word.replace(" ", "_").lower()
         logging.debug("ConceptNet Query: Searching for an edge between %s and the verb primitive %s"
                       % (word, verb_primitive))
@@ -394,7 +392,7 @@ class ConceptNet(KB):
 
     # First check if there is a direct connection via an IsA relation
 
-    def has_IsA_edge(word, concept, verbose=False):
+    def has_IsA_edge(self, word, concept, verbose=False):
         word_text = word.replace(" ", "_").lower()
 
         obj = requests.get(query_prefix + word_text + '?rel=/r/IsA&limit=1000').json()
@@ -405,7 +403,7 @@ class ConceptNet(KB):
             start = edge['start']['label'].lower()
             end = edge['end']['label'].lower()
 
-            if (search_equals(word, start) and isA_equals(concept, end.lower())):
+            if (self.search_equals(word, start) and self.isA_equals(concept, end.lower())):
                 logging.debug("IsA relation found; %s bound to the anchor point %s"
                               % (word, concept))
                 return True
@@ -413,7 +411,7 @@ class ConceptNet(KB):
         return False
 
 
-    def has_edge(word, concept, relation):
+    def has_edge(self, word, concept, relation):
         word_text = word.replace(" ", "_").lower()
 
         obj = requests.get(query_prefix + word_text + '?rel=/r/' + relation + '&limit=1000').json()
@@ -422,14 +420,14 @@ class ConceptNet(KB):
             start = edge['start']['label'].lower()
             end = edge['end']['label'].lower()
 
-            if (search_equals(word, start) and isA_equals(concept, end.lower())):  # == concept.lower()):
+            if (self.search_equals(word, start) and self.isA_equals(concept, end.lower())):  # == concept.lower()):
                 return True
         return False
 
 
     # Phrases don't always count
 
-    def isA_equals(concept, phrase):
+    def isA_equals(self, concept, phrase):
         if concept in phrase:
             return True
         else:
@@ -437,7 +435,7 @@ class ConceptNet(KB):
 
 
 
-    def containsConcept(concept, list):
+    def containsConcept(self, concept, list):
         for item in list:
             if concept in item[0]:
                 return item[0]
@@ -447,7 +445,7 @@ class ConceptNet(KB):
     # TODO - something strange about the query request
     # So hard-coded in a check for the relation
 
-    def search_relation(word, relation):
+    def search_relation(self, word, relation):
         concepts = []
         word_text = word.replace(" ", "_").lower()
         obj = requests.get(query_prefix + word_text + '?rel=/r/' + relation +
@@ -462,7 +460,7 @@ class ConceptNet(KB):
 
     # TODO fix this to not be hardcoded
     # A force that can move things
-    def isConfusion(item):
+    def isConfusion(self, item):
         confusions = ['hurricane', 'storm', 'earthquake']
         if item in confusions:
             return True
@@ -471,8 +469,8 @@ class ConceptNet(KB):
 
 
     # Added for the new primitives
-    def can_move(subject):
-        if has_IsA_edge(subject, 'vehicle') or has_IsA_edge(subject, 'animal'):
+    def can_move(self,subject):
+        if self.has_IsA_edge(subject, 'vehicle') or self.has_IsA_edge(subject, 'animal'):
             return True
         else:
             return False
@@ -480,8 +478,8 @@ class ConceptNet(KB):
 
 
     # Assume this is a list for now
-    def can_propel(contexts):
+    def can_propel(self,contexts):
         for context in contexts:
-            if isConfusion(context):
+            if self.isConfusion(context):
                 return True
         return False
