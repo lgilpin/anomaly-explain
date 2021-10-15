44d1ef5b14252df2a82f61b7b92c08fa0a6ec34c
NathanielKBlan <nblanque@fandm.edu>
2021-10-14 22:53:04 -0400

    Rewrote FactTests as unittest

--- commonsense/tests/FactTests.py
+++ commonsense/tests/FactTests.py
@@ -1,46 +1,38 @@
 from commonsense.logical_classes import Fact
 import pandas as pd
+import unittest
 
-def test_initialization():
-    fact = Fact('penguin', 'isA', 'bird')
-    assert (fact.subject == 'penguin'), "Subject incorrectly initialized in fact"
-    assert (fact.predicate == 'isA'), "Predicate incorrectly initialized in fact"
-    assert (fact.object == 'bird'), "Object incorrectly initialized in fact"
-    assert (fact.reason is None), "Reason incorrectly initialized in fact"
-    assert (fact.score == 1.0), "Score incorrectly initialized in fact"
+class TestFact(unittest.TestCase):
 
-    print("Fact initialization test passed")
+    def test_initialization(self):
+        fact = Fact('penguin', 'isA', 'bird')
+        self.assertTrue(fact.subject == 'penguin', 'Subject incorrectly initialized in fact')
+        self.assertTrue(fact.predicate == 'isA', 'Predicate incorrectly initialized in fact')
+        self.assertTrue(fact.object == 'bird', 'Object incorrectly initialized in fact')
+        self.assertTrue(fact.reason is None, 'Reason incorrectly initialized in fact')
+        self.assertTrue(fact.score == 1.0, 'Score incorrectly initialized in fact')
 
-    pass
+        pass
 
+    def test_get_infix_fact_list(self):
+        fact = Fact('penguin', 'isA', 'bird')
+        infixList = fact.get_infix_fact_list()
 
-def test_get_infix_fact_list():
-    fact = Fact('penguin', 'isA', 'bird')
-    infixList = fact.get_infix_fact_list()
+        self.assertTrue(len(infixList) == 3, 'list of incorrect size returned by get_infix_fact_list')
+        self.assertTrue(infixList[0] == [fact.subject, fact.predicate,
+                                 fact.object], 'Incorrect value, should be of the following form [subject, predicate, object]')
+        self.assertTrue(infixList[1] == fact.reason, 'Incorrect value, should be fact reason')
+        self.assertTrue(infixList[2] == fact.score, 'Incorrect value, should be fact score')
 
-    assert (len(infixList) == 3), "list of incorrect size returned by get_infix_fact_list"
-    assert (infixList[0] == [fact.subject, fact.predicate, fact.object]), "Incorrect value, should be of the following form [subject, predicate, object]"
-    assert (infixList[1] == fact.reason), "Incorrect value, should be fact reason"
-    assert (infixList[2] == fact.score), "Incorrect value, should be fact score"
+        pass
 
-    print("get_infix_fact_list test passed")
+    def test_to_data_frame(self):
+        fact = Fact('penguin', 'isA', 'bird')
+        dataframe = fact.to_data_frame()
 
-    pass
+        self.assertTrue(type(dataframe) is pd.DataFrame, 'to_data_frame returned an incorrect type')
 
-def test_to_data_frame():
-    fact = Fact('penguin', 'isA', 'bird')
-    dataframe = fact.to_data_frame()
-
-    assert(type(dataframe) is pd.DataFrame), "to_data_frame returned an incorrect type"
-
-    print('\n', dataframe, '\n')
-
-    print("to_data_frame test passed")
-
-    pass
+        pass
 
 if __name__ == "__main__":
-    test_initialization()
-    test_get_infix_fact_list()
-    test_to_data_frame()
-    print("All tests passed")
+    unittest.main()
